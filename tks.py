87645abfb65af1dde3c813445cf16690c301d133
Agus Afiantara <afiantara@gmail.com>
2024-02-29 18:08:01 +0700

    update tks.py

--- tks.py
+++ tks.py
@@ -48,6 +48,8 @@ def readpdf_konven(pdf_file):
         print(table)
         table.to_csv('file{}.csv'.format(idx))
         idx+=1
+        
+        
 def get_report_date(year,month):
     my_month = months.index(month) + 1
     cons_tanggal ="{}-{:02}".format(year,my_month)
@@ -62,16 +64,16 @@ def dump_to_db(db_name,table_name,df,report_date):
     # drop 'records table'
     try:
         with db.connect() as conn:
-            query="DELETE FROM {} WHERE REPORT_DATE='{}'".format(table_name,report_date)
+            query="DELETE FROM {} WHERE report_date='{}'".format(table_name,report_date)
             r_set=conn.exec_driver_sql(query)
             conn.commit()
-    except SQLAlchemyError as e:
-        error = str(e)
-        print(error)
+    except Exception as e:
+        print('error')
     else:
         print("No of Records deleted : ",r_set.rowcount)
     finally:
         df.to_sql(table_name, db, if_exists='append')
+        conn.close()
 
 def read_file(pdf_file):
     reader = PyPDF2.PdfReader(pdf_file)
@@ -81,20 +83,67 @@ def read_file(pdf_file):
         table = tabula.io.read_pdf(pdf_file, pages=page_num + 1, stream=True)[0]
         tables.append(table)
     df = pd.concat(tables, ignore_index=True)
-    df.to_csv('./lapkeu.csv')
-    dfLPK = df['LAPORAN POSISI KEUANGAN']
+    
+    #df.to_csv('./lapkeu.csv')
+    
     #sisi aset
-    df0 = df[['Unnamed: 2','Unnamed: 4','Unnamed: 5']].dropna()
+    cols = ['account','2023','2022']
+    dfAsset = df[['Unnamed: 2','Unnamed: 4','Unnamed: 5']].dropna()
+    dfAsset.columns= cols
+    dfAsset['{}'.format(cols[1])]=dfAsset['{}'.format(cols[1])].replace([","," "],"",inplace=True)
     #pendapatan
     dfPendapatan = df[['LAPORAN LABA (RUGI) KOMPREHENSIF','Unnamed: 10']].dropna()
     #pemenuhan tingkat solvability
-    df1= df['TINGKAT KESEHATAN KEUANGAN'].dropna()
-    dflia= df[['Unnamed: 7','Unnamed: 8']].fillna(0)
-    dfTitleLia= df['Unnamed: 7'].fillna(0)
-    print(dfTitleLia.head(25))
-    print(df.columns)
+    dfTks= df['TINGKAT KESEHATAN KEUANGAN'].dropna()
+    #liabilitas & equitas
+    dflia= df[['LAPORAN POSISI KEUANGAN','Unnamed: 7','Unnamed: 8']].fillna(0)
+
+    #print(dfAsset)
+    #print(dfPendapatan)
+    #print(dfTks)
+    #print(dflia)
+    
+    #dfAsset,company_name,yr,yrprev,mth
+    asset_konven(dfAsset,'PT AIA Financial','Konvensional',2023,2022,'Desember')
+    
+    #print(df.columns)
+
+def asset_konven(dfAsset,company_name,jenis,yr,yrprev,mth):
+    cols = ['report_date','company_name','account','jenis','sector','created_date']
+    cols += months
+    #print(f'cols',cols)
+    
+    df = pd.DataFrame(columns=cols)
+    report_date=get_report_date(yr,mth)
+    #print(report_date)
+    
+    df['report_date']=report_date
+    df['company_name']=company_name
+    print(dfAsset)
+    df['account']=dfAsset['account']
+    df['{}'.format(mth)] = dfAsset['{}'.format(yr)].replace([',', ' '],'',inplace=True)
+    df['jenis']=jenis
+    created_date=pd.Timestamp.now
+    df['created_date']= created_date
+    
+    dump_to_db('TKS','ASSET',df,report_date)
+    
+    df = pd.DataFrame(columns=cols)
+    report_date=get_report_date(yrprev,mth)
+    df['report_date']=report_date
+    df['company_name']=company_name
+    df['account']=dfAsset['account']
+    df['{}'.format(mth)] = dfAsset['{}'.format(yrprev)].replace([',', ' '],'',inplace=True)
+    df['jenis']=jenis
+    created_date=pd.Timestamp.now
+    df['created_date']= created_date
+    dump_to_db('TKS','ASSET',df,report_date)
+    
+    print(df)
+
 if __name__=="__main__":
-    pdf_file=f"./pdfs/Published-Conventional-Dec23(unaudited).pdf.coredownload.inline.pdf"
+    
+    pdf_file=f"Published-Conventional-Dec23(unaudited).pdf.coredownload.inline.pdf"
     print(pdf_file)
-    df=read_file(pdf_file)
+    df=read_file(pdf_file) 
     print(df)
